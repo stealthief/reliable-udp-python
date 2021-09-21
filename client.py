5081b2ecf4ad754f3f125f630bfe2e324b0514ac
stealthief <josh.siviour@gmail.com>
2021-09-21 10:19:05 +1000

    First commit

---
+++ client.py
@@ -0,0 +1,131 @@
+import argparse
+import os
+import socket
+import sys
+import struct
+import time
+import random
+import select
+import pickle
+
+MCAST_GRP = "224.1.1.1"
+MCAST_PORT = 5007
+
+class Client:
+    
+    def __init__(self, args):
+        self.mcast_grp = MCAST_GRP
+        self.mcast_port = MCAST_PORT
+        self.data = {}
+        self.args = args
+        self.erased = 0
+
+    def connection(self):
+        self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', self.mcast_port))
+        self.mreq = struct.pack('4sl', socket.inet_aton(self.mcast_grp), socket.INADDR_ANY)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, self.mreq)
+        self.sock.setblocking(0)
+        return True
+
+    def transmit(self, address, packet_type, payload=''):
+        header = bytearray(2)
+        struct.pack_into(
+            '<H',
+            header,
+            0,
+            packet_type
+        )
+        padding = bytearray(self.packet_bytes - (len(header) + len(payload)))
+        packet = header + payload
+
+        self.sock.sendto(packet, address)
+        return True
+
+    def receive(self, missing=[]):
+
+        while True:
+            ready = select.select([self.sock], [], [], 1)
+            if ready[0]:
+                packet, addr = self.sock.recvfrom(1400)
+                symbol = bytearray(packet[18:])
+                packet_type, self.total_bytes, self.packet_bytes, self.total_packets, seq = struct.unpack_from('<HIIII', packet)
+                if packet_type == 1:
+                    self.transmit(addr, 1, bytearray('Client1', 'utf-8'))
+                    break
+                elif packet_type == 2:
+                    self.data[seq] = symbol
+                    try:
+                        missing.remove(seq)
+                    except:
+                        pass
+                elif packet_type == 3:
+                    self.erased += len(missing)
+                    res = pickle.dumps(missing)
+                    self.transmit(addr, 3, res)
+                elif packet_type == 4:
+                    break
+        return True
+
+
+def arguments():
+        parser = argparse.ArgumentParser(description=main.__doc__)
+        parser.add_argument(
+            "--output-file",
+            type=str,
+            help="Path to the file which should be received.",
+            default="output_file",
+        )
+
+        parser.add_argument(
+            "--ip", type=str, help="The IP address to use.", default=MCAST_GRP
+        )
+
+        parser.add_argument("--port", type=int, help="The port to use.", default=MCAST_PORT)
+
+        parser.add_argument(
+            "--dry-run", action="store_true", help="Run without network use."
+        )
+
+        args = parser.parse_args()
+        return args
+
+def main():
+    args = arguments()
+    c = Client(args)
+    c.connection()
+
+    start = time.time()
+    print("Awaiting engineering packet...")
+    eng_pkt = False
+    complete = False
+    try:
+        # Receive eng packet and ack, then receive file
+        while not eng_pkt:
+            if c.receive():
+                eng_pkt = True
+
+        print("Attempting to receive data...")
+        while not complete:
+            missing = list(range(20))
+            if c.receive(missing):
+                complete = True
+
+        f = open(c.args.output_file, "wb")
+        for k in range(len(c.data)):
+            f.write(c.data[k])
+        f.close()
+        print("File received")
+        delta = time.time() - start
+
+        print(f"Throughput: {c.total_bytes / delta} Bytes/s")
+        print(f"Total erasure: {c.erased / c.total_packets}")
+
+    except KeyboardInterrupt:
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
+    
+
---
+++ server.py
@@ -0,0 +1,169 @@
+import argparse
+import os
+import socket
+import sys
+import struct
+import time
+import random
+import select
+import pickle
+
+MCAST_GRP = "224.1.1.1"
+MCAST_PORT = 5007
+
+class Server:
+    
+    def __init__(self, args):
+        self.mcast_grp = MCAST_GRP
+        self.mcast_port = MCAST_PORT
+        self.clients = []
+        self.data = {}
+        self.address = (self.mcast_grp, self.mcast_port)
+        self.args = args
+        file_stats = os.stat(self.args.file_path)
+        self.total_bytes = file_stats.st_size
+        self.packet_bytes = 1400
+        self.total_packets = self.total_bytes // self.packet_bytes + 1
+        self.gen_size = 20
+
+    def connection(self):
+        self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        self.sock.setblocking(0)
+        return True
+
+    def open_file(self):
+        if not os.path.isfile(self.args.file_path):
+            print(f"{self.args.file_path} is not a valid file.")
+            sys.exit(1)
+        else:
+            self.f = open(os.path.expanduser(self.args.file_path), 'rb')
+            return True
+
+    def get_data(self, seq):
+        self.data[seq] = self.f.read(self.packet_bytes-18)
+        return self.data[seq]
+
+    def create_packet(self, packet_type, seq=0, payload=b''):
+
+        header = bytearray(18)
+        struct.pack_into(
+            '<HIIII',
+            header,
+            0,
+            packet_type,
+            self.total_bytes,
+            self.packet_bytes,
+            self.total_packets,
+            seq
+        )
+        padding = bytearray(self.packet_bytes - (len(header) + len(payload)))
+        packet = header + payload + padding
+
+        return packet
+
+    def transmit(self, packet):
+        while True:
+            ready = select.select([], [self.sock], [], 1)
+            if ready[1]:
+                self.sock.sendto(packet, self.address)
+                break
+        return True
+
+    def receive(self):
+        packet_type = None
+        missing = []
+
+        while True:
+            ready = select.select([self.sock], [], [], 1)
+            if ready[0]:
+                packet = self.sock.recv(1400)
+                symbol = bytearray(packet[2:])
+                packet_type, = struct.unpack_from('<H', packet)
+                if packet_type == 1:
+                    if symbol not in self.clients:
+                        self.clients.append(symbol)
+                    break
+                elif packet_type == 3:
+                    res = pickle.loads(symbol)
+                    for i in res:
+                        self.transmit(self.create_packet(2, i, self.data[i]))
+                    break          
+
+        return True
+
+def arguments():
+        parser = argparse.ArgumentParser(description=main.__doc__)
+
+        """The parser takes a path to a file as input."""
+        parser.add_argument(
+            "--file-path",
+            type=str,
+            help="Path to the file which should be sent.",
+            default=os.path.realpath(__file__),
+        )
+
+        """The parser takes the target IP-address as input."""
+        parser.add_argument(
+            "--ip", type=str, help="The IP address to send to.", default=MCAST_GRP
+        )
+
+        """The parser takes the target port as input."""
+        parser.add_argument(
+            "--port", type=int, help="The port to send to.", default=MCAST_PORT
+        )
+
+        """One can tell the parser to run without using the network."""
+        parser.add_argument(
+            "--dry-run", action="store_true", help="Run without network use."
+        )
+        args = parser.parse_args()
+        return args
+
+def main():
+    args = arguments()
+    s = Server(args)
+    s.connection()
+    f = s.open_file()
+    seq = 0
+    eng = False
+    complete = False
+    # Send eng packets
+
+    try:
+        eng = s.create_packet(1)
+        for i in range(1):
+            s.transmit(eng)
+        print("Engineering packet sent")
+
+        # Will this keep trying to receive response??
+        while not eng:
+            if s.receive():
+                eng = True
+        #print(f"Number of clients is: {s.clients}")
+
+        print(f"Total packets: {s.total_packets}\nNumber of gens: {s.total_packets / s.gen_size}")
+        num_gens = s.total_packets // s.gen_size + 10
+        print(f"Number of gens calced: {num_gens}")
+        # Initial transmission
+        for i in range(num_gens):
+            packets = {}
+            for j in range(s.gen_size):
+                data = s.get_data(seq)
+                packet = s.create_packet(2, seq, data)
+                s.transmit(packet)
+                seq += 1
+            for i in range(3):
+                s.transmit(s.create_packet(3))
+
+            s.receive()
+        
+        s.transmit(s.create_packet(4))
+
+        print("File transfer complete.")
+    except KeyboardInterrupt:
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
+
